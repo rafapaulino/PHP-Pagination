4e87d76b08707583ffbd53c0b4c1a3580e9b892e
Rafael Brigagao Paulino <rafael.paulino@FIESP.SP>
2017-06-12 16:50:21 -0300

    Making the Tests Run

---
+++ Tests/PaginationTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Pagination\Tests;
+
+class PaginationTest extends \PHPUnit_Framework_TestCase {
+
+	public function testTrueIsTrue()
+	{
+	    $foo = true;
+	    $this->assertTrue($foo);
+	}
+	
+}
---
+++ src/Pagination.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Pagination;
+
+class Pagination {
+	
+}
