9a6e3e0b0924611185ee6b24c5f4c8e7c32742a4
Rafael Brigagao Paulino <rafael.paulino@FIESP.SP>
2017-06-14 11:58:03 -0300

    Inserting the main properties of the paging class

--- Tests/PaginationTest.php
+++ Tests/PaginationTest.php
@@ -6,10 +6,22 @@ use Pagination\Pagination;
 
 class PaginationTest extends \PHPUnit_Framework_TestCase {
 
+	protected $_pagination;
+
+    protected function setUp()
+    {
+        $this->_pagination = new Pagination;
+    }
+
 	public function testTrueIsTrue()
 	{
 	    $foo = true;
 	    $this->assertTrue($foo);
+	}
+
+	public function testInstanceOfPagination() 
+	{
+		$this->assertInstanceOf(Pagination::class, $this->_pagination);
 	}
 	
 }
--- src/Pagination.php
+++ src/Pagination.php
@@ -3,5 +3,32 @@
 namespace Pagination;
 
 class Pagination {
-	
+
+    protected $_page;
+    protected $_recordsPerPage;
+    protected $_totalOfResults;
+
+    public function getPage(): int {
+        return $this->_page;
+    }
+
+    public function setPage(int $page) {
+        $this->_page = $page;
+    }
+
+    public function getRecordsPerPage(): int {
+        return $this->_recordsPerPage;
+    }
+
+    public function setRecordsPerPage(int $recordsPerPage) {
+        $this->_recordsPerPage = $recordsPerPage;
+    }
+
+    public function getTotalOfResults(): int {
+        return $this->_totalOfResults;
+    }
+    
+    public function setTotalOfResults(int $totalOfResults) {
+        $this->_totalOfResults = $totalOfResults;
+    }     
 }
